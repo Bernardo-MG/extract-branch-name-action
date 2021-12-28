577e76854f448adca48e6ef623329941f261d612
Bernardo <programming@bernardomg.com>
2021-12-28 23:37:49 +0100

    Renamed output

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 # Fails if any commands returns a non-zero value
 set -e
 
-##[set-output name=branch;]$(echo ${GITHUB_REF#refs/heads/})
+##[set-output name=branchName;]$(echo ${GITHUB_REF#refs/heads/})
 
 exit 0
